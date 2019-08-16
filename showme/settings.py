5da45f9a648fc2501a00ef356348b406359a414b
Unknown <781637982@qq.com>
2019-08-16 21:45:37 +0800

    添加blog网页
    简单制作了blog网页，但是没有进行跳转设置，下一步继续完善

--- showme/settings.py
+++ showme/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 我的APP
     'gallary.apps.GallaryConfig',
+    'blog',
 ]
 
 MIDDLEWARE = [
