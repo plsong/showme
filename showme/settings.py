ae7db92a43b5e13e93dc61a064a857bd826b8ae0
Unknown <781637982@qq.com>
2019-08-16 21:08:00 +0800

    利用bootstrap设计主页风格
    将主页按照bootstap上的模板，进行设计

--- showme/settings.py
+++ showme/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gallary.apps.GallaryConfig',#我的APP
+    'gallary.apps.GallaryConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
