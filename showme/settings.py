95544b60ce40e11e9fc4cb066d4fe6d4b49c6e65
Unknown <781637982@qq.com>
2019-08-17 10:02:24 +0800

    添加个人简历
    在主页添加了个人简历，链接到了静态文件中

--- showme/settings.py
+++ showme/settings.py
@@ -118,8 +118,16 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
+
+
+
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'showme/static/')
+
+]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
